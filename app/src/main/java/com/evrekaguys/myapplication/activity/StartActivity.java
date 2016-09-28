cce5952ef5a116bda2e8201ccd5264d5682beb29
Mustafa <mustafasubasi18@gmail.com>
2016-09-28 23:46:50 +0300

    Emua Project
    Image problem was fixed.

--- app/src/main/java/com/evrekaguys/myapplication/activity/StartActivity.java
+++ app/src/main/java/com/evrekaguys/myapplication/activity/StartActivity.java
@@ -1,8 +1,15 @@
 package com.evrekaguys.myapplication.activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,6 +28,7 @@ import java.util.Set;
 
 public class StartActivity extends BaseActivity {
 
+    View view;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -29,14 +37,49 @@ public class StartActivity extends BaseActivity {
         getSupportActionBar().hide();
 
         ImageView img = (ImageView) findViewById(R.id.start);
+
         img.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                goTabletMenu(v);
+                view = v;
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if( PackageManager.PERMISSION_GRANTED != checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                       requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},2909);
+                    }else{
+                        goTabletMenu(v);
+                    }
+                }else{
+                    goTabletMenu(v);
+                }
                 return true;
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+       switch (requestCode){
+           case 2909:{
+               if(grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+                   goTabletMenu(view);
+               }
+               return;
+           }
+       }
+    }
+
+    public boolean isStoragePermissionGranted(){
+        if(Build.VERSION.SDK_INT >= 23){
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                return true;
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                return false;
+            }
+        }else{
+            return true;
+        }
     }
 
     public void goTabletMenu(View v) {
